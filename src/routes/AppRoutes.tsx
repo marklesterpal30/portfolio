e1d6a3419b6e8cfdc995475e5ac40cdb224db5c7
Mark Lester <marklestermarananpal@gmail.com>
2025-09-06 16:42:02 +0800

    adjust the rout in the approutes

--- src/routes/AppRoutes.tsx
+++ src/routes/AppRoutes.tsx
@@ -2,13 +2,13 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Main from "../pages/Main";
 
 const AppRoutes = () => {
-  return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Main />} />
-      </Routes>
-    </Router>
-  );
+	return (
+		<Router>
+			<Routes>
+				<Route path="/porfolio" element={<Main />} />
+			</Routes>
+		</Router>
+	);
 };
 
 export default AppRoutes;
