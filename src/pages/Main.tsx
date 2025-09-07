49b08951413a58f7cfad5d6da9ea88ef12879610
Mark Lester <marklestermarananpal@gmail.com>
2025-09-08 01:23:48 +0800

    add a connect with me button that shows my social media and github account

--- src/pages/Main.tsx
+++ src/pages/Main.tsx
@@ -4,16 +4,18 @@ import Services from "./Services";
 import Projects from "./Projects";
 import Contact from "./Contact";
 import Testimonial from "./Testimonial";
+import SocialConnectButton from "../components/SocialConnectButton";
 
 const Main = () => {
   return (
-    <div>
+    <div className="relative">
       <Hero />
       <About />
       <Services />
       <Projects />
       <Testimonial />
       <Contact />
+      <SocialConnectButton />
     </div>
   );
 };
